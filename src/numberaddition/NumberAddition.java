db3eab2916d541b1a0485e781defab4733b9088a
williamclifford94 <92854245+williamclifford94@users.noreply.github.com>
2024-01-23 10:21:00 +0000

    My first commit: added some numbers (kinda)

---
+++ src/numberaddition/NumberAddition.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package numberaddition;
+
+/**
+ *
+ * @author WilliamClifford
+ */
+public class NumberAddition {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       //System.out.println("Hello World");
+       my.NumberAdditionUI myUI = new my.NumberAdditionUI();
+       
+    }
+    
+}
